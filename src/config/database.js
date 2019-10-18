8a1e0641c626ffb79988ddae2b1ee1ac13492bca
Marcelo Macedo <marcelo3macedo@gmail.com>
2019-10-17 21:05:14 -0300

    Adding User Controller

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'marco661',
+    database: 'go_barber',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+};
