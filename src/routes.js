5b2ee88b1a98b127c89c79020293d6e800420281
Marcelo Macedo <marcelo3macedo@gmail.com>
2019-10-20 17:29:36 -0300

    List with Pagination

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import UserController from './app/controllers/UserController';
 import FileController from './app/controllers/FileController';
 import SessionController from './app/controllers/SessionController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
@@ -19,6 +20,9 @@ routes.use(authMiddleware);
 routes.put("/users", UserController.update);
 
 routes.post("/files", upload.single('file'), FileController.store);
+
+routes.get("/appointments", AppointmentController.index);
+routes.post("/appointments", AppointmentController.store);
 
 routes.get("/providers", ProviderController.index);
 
