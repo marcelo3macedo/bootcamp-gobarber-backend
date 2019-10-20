d30562adde06bd8d2b31e35af54b948afab6d5d2
Marcelo Macedo <marcelo3macedo@gmail.com>
2019-10-20 17:50:08 -0300

    Schedule list

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import FileController from './app/controllers/FileController';
 import SessionController from './app/controllers/SessionController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
@@ -23,6 +24,8 @@ routes.post("/files", upload.single('file'), FileController.store);
 
 routes.get("/appointments", AppointmentController.index);
 routes.post("/appointments", AppointmentController.store);
+
+routes.get("/schedule", ScheduleController.index);
 
 routes.get("/providers", ProviderController.index);
 
