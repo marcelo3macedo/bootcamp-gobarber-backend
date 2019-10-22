f5ad42a3e037c342d03e5939aa78fd2c07b4137e
Marcelo Macedo <marcelo3macedo@gmail.com>
2019-10-21 21:17:57 -0300

    nodemailer

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -2,7 +2,8 @@ import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
-import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import Mail from '../../lib/Mail';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 import * as Yup from 'yup';
 
@@ -90,6 +91,58 @@ class AppointmentController {
             content: `Novo agendamento de ${user.name} para o ${formattedDate}`,
             user: provider_id,
         });        
+
+        return res.json(appointment);
+    }
+
+    async delete(req, res) {
+        const appointment = await Appointment.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    as: 'provider',
+                    attributes: [ 'name', 'email']
+                },
+                {
+                    model: User,
+                    as: 'user',
+                    attributes: [ 'name', 'email']
+                }
+            ]
+        });
+
+        if (appointment.user_id != req.userId) {
+            return res.status(401).json({
+                error: 'You dont have permission to cancel'
+            }); 
+        }
+
+        const dateWithSub = subHours(appointment.date, 2);
+
+        if (isBefore(dateWithSub, new Date())) {
+            return res.status(401).json({
+                error: 'You only can cancel appointments 2 hours in advance.'
+            }); 
+        }
+
+        appointment.canceled_at = new Date();
+
+        await appointment.save();
+
+        await Mail.sendMail({
+            to: `${appointment.provider.name} <${appointment.provider.email}>`,
+            subject: 'Agendamento Cancelado',
+            template: 'cancellation',
+            context: {
+                provider: appointment.provider.name,
+                user: appointment.user.name,
+                date: format(
+                    appointment.date,
+                    "'dia' dd 'de' MMMM ', às ' H:mm'h'",
+                    { locale: pt }
+                ) 
+            }
+        });
 
         return res.json(appointment);
     }
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
+    auth: {
+        user: '8d1553cfdc5b12',
+        pass: 'f70bcf30824088'
+    },
+    default: {
+        from: 'Equipe Gobarber <noreply@gobarber.com>'
+    }
+}
